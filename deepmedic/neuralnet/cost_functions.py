WIP: updated cost function to take into account correct batch size also updated the mse loss to be between logits and not softmax outputs.

deepmedic/neuralnet/cost_functions.py
@@ -44,7 +44,7 @@ def x_entr_mean_teacher( p_y_given_x_train, y_gt, weightPerClass, y_data, eps=1e
     num_samples = tf.cast( tf.reduce_prod( tf.shape(y_gt) ), "float32")
 
     batches_to_include = tf.cast( tf.math.logical_not( tf.math.reduce_any(y_data, axis=1) ), "float32" )
-    relevant_batch = tf.reduce_sum(batches_to_include)
+    relevant_batch = tf.cast( tf.reduce_sum(batches_to_include), "float32") * tf.cast( tf.reduce_prod( tf.shape(y_gt)[1:] ), "float32")
     batches_to_include = tf.reshape( batches_to_include, shape=[-1,1,1,1,1] ) 
     
     return - (1./ relevant_batch) * tf.reduce_sum( weighted_log_p_y_given_x_train * y_one_hot * batches_to_include )